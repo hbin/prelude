2ad364311865e7b4aba417d6e127c8af6e6d4dfe
Huang Bin <huangbin88@foxmail.com>
2013-12-16 10:41:11 +0800

    Customize modules

--- personal/hbin-modules.el
+++ personal/hbin-modules.el
@@ -1,39 +1,28 @@
 ;;; hbin-modules --- customize modules
-;;
-;; Copyright (C) 2012-2013 Huang Bin
-;;
-;; Author: Huang Bin <embrace.hbin@gmail.com>
-;; Version: 1.0.0
 
-;;; Commentary:
-
-;; This file is not part of GNU Emacs.
-
-;;; Code:
+(require 'prelude-ido)
+;; (require 'prelude-helm)
 
 (require 'prelude-c)
 ;; (require 'prelude-clojure)
 (require 'prelude-coffee)
-(require 'prelude-common-lisp)
+;; (require 'prelude-common-lisp)
 (require 'prelude-css)
 (require 'prelude-emacs-lisp)
-;; (require 'prelude-erc)
+;; (require 'prelude-erc) ;; Emacs IRC client
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
+(require 'prelude-key-chord)
 ;; (require 'prelude-latex)
-(require 'prelude-lisp)
-(require 'prelude-markdown)
+;; (require 'prelude-lisp)
 ;; (require 'prelude-mediawiki)
-(require 'prelude-org)
+(require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
 ;; (require 'prelude-perl)
-;; (require 'prelude-python)
+(require 'prelude-python)
 (require 'prelude-ruby)
 ;; (require 'prelude-scala)
 ;; (require 'prelude-scheme)
 (require 'prelude-scss)
-(require 'prelude-web)
+(require 'prelude-web) ;; Emacs mode for web templates
 (require 'prelude-xml)
-
-(provide 'hbin-modules)
-;;; hbin-modules.el ends here
