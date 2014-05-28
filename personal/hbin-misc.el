ec07bb268c4c4c2e425b6cfdd05d3c7eb944bcd2
Huang Bin <huangbin88@foxmail.com>
2014-05-28 15:12:45 +0800

    Keybindings for `projectile`.

--- personal/hbin-misc.el
+++ personal/hbin-misc.el
@@ -13,41 +13,11 @@
 
 ;; Miscellaneous Packages
 (prelude-require-packages
- '(popwin highlight-symbol textmate multiple-cursors whole-line-or-region key-chord))
+ '(popwin whole-line-or-region key-chord))
 
 ;;; Disable guru-mode completely
 (require 'guru-mode)
 (setq prelude-guru nil)
-
-;; Expand region
-(require 'expand-region)
-(global-set-key (kbd "M-h") 'er/expand-region)
-
-;;; Textmate
-(require 'textmate)
-(setq textmate-use-file-cache nil)
-(global-set-key (kbd "M-t") 'textmate-goto-file)
-(global-set-key (kbd "M-T") 'textmate-goto-symbol)
-(global-set-key (kbd "M-]") 'textmate-shift-right)
-(global-set-key (kbd "M-[") 'textmate-shift-left)
-
-;;; Highlight symbols
-(require 'highlight-symbol)
-(global-set-key (kbd "M-m") 'highlight-symbol-at-point)
-(global-set-key (kbd "M-M") 'highlight-symbol-remove-all)
-(global-set-key (kbd "M-n") 'highlight-symbol-next)
-(global-set-key (kbd "M-p") 'highlight-symbol-prev)
-(global-set-key (kbd "M-N") 'highlight-symbol-next-in-defun)
-(global-set-key (kbd "M-P") 'highlight-symbol-prev-in-defun)
-
-;;; Multiple Cursors
-(require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "S-<down>") 'mc/mark-next-lines)
-(global-set-key (kbd "S-<up>") 'mc/mark-previous-lines)
-(global-set-key (kbd "C-*") 'mc/mark-all-symbols-like-this)
-(global-set-key (kbd "C-<") 'mc/mark-previous-symbol-like-this)
-(global-set-key (kbd "C->") 'mc/mark-next-symbol-like-this)
 
 ;;; Whole line or region
 (require 'whole-line-or-region)
