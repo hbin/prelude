36a083f82351c428a48a22600bab62c9f0c78b84
Bin Huang <huangbin88@foxmail.com>
2014-05-30 13:46:10 +0800

    Extract settings for projectile to a config file.

---
+++ personal/misc-projectile.el
@@ -0,0 +1,47 @@
+;; misc-projectile.el --- Configuration for Projectile
+;;
+;; Copyright (C) 2012-2014 Huang Bin
+;;
+;; Author: Huang Bin <huangbin88@foxmail.com>
+;; Version: 1.0.0
+
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This file is not part of GNU Emacs.
+
+;;; Code:
+(prelude-require-packages '(projectile))
+(require 'projectile)
+
+(defun projectile-regenerate-tags-maybe ()
+  "Generate tags if in the project."
+  (when (projectile-project-p)
+    (projectile-regenerate-tags)
+    (ac-etags-clear-cache)))
+
+(add-hook 'after-save-hook 'projectile-regenerate-tags-maybe)
+(add-hook 'pe/before-tree-lookup-hook 'projectile-regenerate-tags-maybe)
+;; (add-hook 'find-file-hook 'projectile-regenerate-tags-maybe)
+
+;;; Keybindings
+(define-key prelude-mode-map [?\s-d] nil)
+(define-key prelude-mode-map [?\s-p] nil)
+(define-key prelude-mode-map [?\s-f] nil)
+(define-key prelude-mode-map [?\s-g] nil)
+
+(define-key projectile-mode-map (kbd "M-t") 'projectile-find-file)
+(define-key projectile-mode-map (kbd "M-T") 'projectile-find-file-other-window)
+(define-key projectile-mode-map (kbd "M-g") 'projectile-grep)
+(define-key projectile-mode-map (kbd "M-R") 'projectile-replace)
+(define-key projectile-mode-map (kbd "C-x d") 'projectile-find-dir)
+(define-key projectile-mode-map (kbd "C-x D") 'projectile-dired)
+(define-key projectile-mode-map (kbd "C-x C-i") 'projectile-find-tag)
+(define-key projectile-mode-map (kbd "C-x C-p") 'projectile-switch-project)
+
+;;;;;;;;;;;;;;;;;;;;;;;;
+
+(provide 'misc-projectile)
+
+;;; misc-projectile.el ends here
