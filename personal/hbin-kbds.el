30c190a2f503bded0f57ea9e00283e788b9713bb
Huang Bin <huangbin88@foxmail.com>
2014-06-03 17:07:30 +0800

    kbds for windowmove

--- personal/hbin-kbds.el
+++ personal/hbin-kbds.el
@@ -98,10 +98,26 @@
 (global-set-key (kbd "C-<") 'mc/mark-previous-symbol-like-this)
 (global-set-key (kbd "C->") 'mc/mark-next-symbol-like-this)
 
+;; Goto line
+(global-set-key (kbd "s-g") 'goto-line)
+
 ;;; Winner Mode
 (winner-mode 1)
 (global-set-key (kbd "C-x <left>") 'winner-undo)
 (global-set-key (kbd "C-x <right>") 'winner-redo)
+
+(require 'windmove)
+(define-key prelude-mode-map (kbd "s-h")  'windmove-left)
+(define-key prelude-mode-map (kbd "s-l") 'windmove-right)
+(define-key prelude-mode-map (kbd "s-k")    'windmove-up)
+(define-key prelude-mode-map (kbd "s-j")  'windmove-down)
+
+(define-key prelude-mode-map (kbd "s-+") 'enlarge-window-horizontally)
+(define-key prelude-mode-map (kbd "s-=") 'enlarge-window-horizontally)
+(define-key prelude-mode-map (kbd "s--") 'shrink-window-horizontally)
+(define-key prelude-mode-map (kbd "C-s-+") 'enlarge-window)
+(define-key prelude-mode-map (kbd "C-s-=") 'enlarge-window)
+(define-key prelude-mode-map (kbd "C-s--") 'shrink-window)
 
 (provide 'hbin-kbds)
 ;;; hbin-kbds.el ends here
