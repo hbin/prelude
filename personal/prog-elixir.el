90f5f547aba728ee16bc1a2579a509bd4801a0e6
Huang Bin <huangbin88@foxmail.com>
2017-08-14 18:15:46 +0800

    Add alchemist mode

---
+++ personal/prog-elixir.el
@@ -0,0 +1,18 @@
+;; prog-elixir.el --- Enhance Programming Elixir
+;;
+;; Copyright (C) 2012-2017 Huang Bin
+;;
+;; Author: Huang Bin <huangbin88@foxmail.com>
+;; Version: 1.0.0
+
+;;; Commentary:
+
+;; This file is not part of GNU Emacs.
+
+;;; Code:
+
+(prelude-require-packages '(elixir-mode alchemist))
+(add-to-list 'elixir-mode-hook (alchemist-mode +1))
+
+(provide 'prog-elixir)
+;;; prog-elixir.el ends here
